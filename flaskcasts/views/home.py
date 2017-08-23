cec4e557c54c5c14f87d72940b0673b0eefede47
Eddie Sherman <eddie.sherman@gmail.com>
2017-08-23 12:34:21 -0400

    Added some templates

--- flaskcasts/views/home.py
+++ flaskcasts/views/home.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 home = Blueprint('home', __name__)
 
 @home.route('/')
 def index():
-    return "Welcome to the Index"
+    return render_template('home/index.html')
