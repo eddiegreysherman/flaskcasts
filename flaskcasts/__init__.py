f986594fe6bd97b6ec88df8c5c26bdfd1633e39a
Eddie Sherman <eddie.sherman@gmail.com>
2017-09-06 07:25:52 -0400

    configuration settings

--- flaskcasts/__init__.py
+++ flaskcasts/__init__.py
@@ -2,9 +2,9 @@ from flask import Flask
 from flask_pymongo import PyMongo
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
-app.secret_key = app.config['SECRET_KEY']
+app.config.from_pyfile('config.py')
 
 mongo = PyMongo(app)
 
